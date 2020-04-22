113a80782059e84c45527b08c119781e6f8c2084
Mkhira <45259648+Mkhira@users.noreply.github.com>
2020-04-22 08:38:24 +0200

    Graduation

--- lib/Screens/BlodDonation.dart
+++ lib/Screens/BlodDonation.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../style/constent.dart';
-import '../wedgit/headersWedgit.dart';
+import '../wedgit/BloodHeaderWidget.dart';
 import '../wedgit/caruselWedgit.dart';
 import '../wedgit/bloodWedgit.dart';
 
@@ -11,55 +11,7 @@ class BloodDonation extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
       backgroundColor: kMainColor,
-      body: SingleChildScrollView(
-        child: Container(
-          height: 1000,
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                top: 280,
-                left: 0,
-                child: Container(
-                  height: 400,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        bottomRight: Radius.circular(20)),
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      left: 10,
-                      right: 10,
-                    ),
-                    child: BloodListBuilder(),
-                  ),
-                ),
-              ),
-              topHeader(),
-              Positioned(
-                bottom: 280,
-                left: 20,
-                child: Text(
-                  "Blood Donation Events",
-                  style: kTitleHeader,
-                ),
-              ),
-              Positioned(
-                bottom: -20,
-                left: 0,
-                child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 300,
-                    child:
-                       caruselBuilder()
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: CustomScrollView(),
     ));
   }
 }
