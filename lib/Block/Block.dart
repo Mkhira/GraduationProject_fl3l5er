cb3445ce6bac99bd4c23e8dea5d778596caf4881
Mkhira <45259648+Mkhira@users.noreply.github.com>
2020-07-21 17:52:00 +0200

    data

--- lib/Block/Block.dart
+++ lib/Block/Block.dart
@@ -14,7 +14,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '../wedgit/fireBaseError wedgit.dart';
 import '../models/eventModel.dart';
 import '../service/repostry.dart';
 import 'package:rxdart/futures.dart';
